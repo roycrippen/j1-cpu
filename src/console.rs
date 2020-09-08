b52c9806cb94974c2444429a1cc82594d232c9bd
roycrippen <roycrippen@gmail.com>
2020-09-08 18:48:43 -0500

    fix console bugs for mock console

--- src/console.rs
+++ src/console.rs
@@ -40,12 +40,10 @@ impl<T: IO> Console<T> {
 pub trait IO {
     fn new() -> Self;
     fn load_buf(&mut self, xs: &mut [u8]);
-    fn buf_len(&self) -> usize;
+    fn buf_has_char(&self) -> u16;
     fn read_byte(&mut self) -> Option<u8>;
-    fn read_all_bytes(&mut self) -> Vec<u8>;
     fn write_byte(&mut self, v: u8);
 }
-
 
 pub type MockConsole = Cursor<Vec<u8>>;
 
@@ -57,27 +55,21 @@ impl IO for MockConsole {
         self.set_position(0);
     }
 
-    fn buf_len(&self) -> usize {
-        self.get_ref().len() - self.position() as usize
+    fn buf_has_char(&self) -> u16 {
+        if self.get_ref().len() as u64 - self.position() > 0 { 1 } else { 0 }
     }
 
     fn read_byte(&mut self) -> Option<u8> {
+        if self.get_ref().len() == 0 {
+            return None
+        }
+        let mut ar1 = [0u8];
+        let _ = self.read_exact(&mut ar1);
         if self.position() == self.get_ref().len() as u64 {
-            None
-        } else {
-            let mut ar1 = [0u8];
-            let _ = self.read_exact(&mut ar1);
-            Some(ar1[0])
+            self.get_mut().clear();
+            self.set_position(0);
         }
-    }
-
-    fn read_all_bytes(&mut self) -> Vec<u8> {
-        self.set_position(0);
-        let mut xs: Vec<u8> = Vec::new();
-        while let Some(v) = self.read_byte() {
-            xs.push(v);
-        }
-        xs
+        Some(ar1[0])
     }
 
     fn write_byte(&mut self, v: u8) {
@@ -94,15 +86,10 @@ mod tests {
         let console = &mut Console::<MockConsole>::new(true);
 
         let mut cmds: Vec<u8> = "1 2 + .s\n".bytes().collect();
-        console.buf.load_buf(&mut cmds);
-        assert_eq!(cmds.len(), console.buf.buf_len());
+        console.read(&mut cmds);
+        assert_eq!(cmds.len(), console.buf.get_ref().len());
         assert_eq!(0, console.buf.position());
         // println!("buff len = {:?}, buff pos = {}", console.io.get_ref().len(), console.io.position());
-
-        let xs = console.buf.read_all_bytes();
-        assert_eq!(cmds, xs);
-        assert_eq!(None, console.buf.read_byte());
-        assert_eq!(console.buf.buf_len(), 0);
     }
 
     #[test]
